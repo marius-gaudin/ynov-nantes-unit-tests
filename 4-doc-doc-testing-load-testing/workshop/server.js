2e6a7c5368d15923442b93bfe881f7cf1e994680
bastien aubry <pma.bastien.aubry@gmail.com>
2022-04-29 15:28:32 +0200

    feat(doc): integration de la doc open api et swagger

--- 4-doc-doc-testing-load-testing/workshop/server.js
+++ 4-doc-doc-testing-load-testing/workshop/server.js
@@ -1,13 +1,153 @@
 const express = require('express');
-const path = require('path');
 const bodyParser = require('body-parser');
-
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+
+const options = {
+  "swagger": "2.0",
+  "info": {
+    "description": "Three CRU operations : GET, POST, PUT for Todo object",
+    "version": "1.0.0",
+    "title": "Todo api",
+    "contact": {
+      "email": "bastien.aubry@ynov"
+    }
+  },
+  "basePath": "/",
+  "tags": [
+    {
+      "name": "todo",
+      "description": "Three CRU operations : GET, POST, PUT for Todo object",
+      "externalDocs": {
+        "description": "Find out more",
+        "url": "http://localhost:5000"
+      }
+    }
+  ],
+  "paths": {
+    "/todo": {
+      "post": {
+        "tags": [
+          "todo"
+        ],
+        "summary": "Add a new Todo to the store",
+        "description": "",
+        "operationId": "addtodo",
+        "consumes": [
+          "application/json"
+        ],
+        "produces": [
+          "application/json"
+        ],
+        "parameters": [
+          {
+            "in": "body",
+            "name": "body",
+            "description": "todo object to save",
+            "required": true,
+            "schema": {
+              "$ref": "#/definitions/todo"
+            }
+          }
+        ],
+        "responses": {
+          "201": {
+            "description": "Sucess"
+          },
+          "400": {
+            "description": "Bad request"
+          }
+        }
+      },
+      "get": {
+        "tags": [
+          "todo"
+        ],
+        "summary": "getthe lsit of todos",
+        "description": "",
+        "operationId": "getTodos",
+        "produces": [
+          "application/json"
+        ],
+        "responses": {
+          "200": {
+            "description": "Success"
+          },
+          "400": {
+            "description": "Bad request"
+          }
+        }
+      }
+    },
+    "/todo/{_id}": {
+      "patch": {
+        "tags": [
+          "todo"
+        ],
+        "summary": "Update a todo to make it done",
+        "description": "",
+        "operationId": "updateTodo",
+        "consumes": [
+          "application/json"
+        ],
+        "produces": [
+          "application/json"
+        ],
+        "parameters": [
+          {
+            "name": "_id",
+            "in": "path",
+            "description": "ID of todo to update",
+            "required": true,
+            "type": "string",
+            "format": "int64"
+          }
+        ],
+        "responses": {
+          "200": {
+            "description": "Success"
+          },
+          "400": {
+            "description": "Bad request"
+          }
+        }
+      }
+    }
+  },
+  "definitions": {
+    "todo": {
+      "type": "object",
+      "required": [
+        "text",
+        "done"
+      ],
+      "properties": {
+        "text": {
+          "type": "string"
+        },
+        "done": {
+          "type": "boolean"
+        }
+      },
+      "xml": {
+        "name": "todo"
+      }
+    }
+  }
+}
+
+app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(options));
+
+
+
 app.use(express.static(__dirname + '/public'));
+
 
 app.get('/todo', (req, res) => {
   ToDo.find()
@@ -38,5 +178,10 @@ const DB_URI = 'mongodb://mongo:27017/toDoApp';
 
 mongoose.connect(DB_URI).then(() => {
   console.log('Listening on port: ' + PORT);
+    
   app.listen(PORT);
 });
+
+
+
+// OpenAPI routes
